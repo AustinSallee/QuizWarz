b6811d116e1d3409a24db62d3449f109c54e004d
Austin <Stephen.sallee698@topper.wku.edu>
2018-01-30 15:55:38 -0800

    Assets and Animations
    Organized assets and created placeholder animations with controller scrip for the battle scene

---
+++ Assets/Scripts/KnightBattleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightBattleScript : MonoBehaviour {
+
+    Animator anim;
+    int attackHash = Animator.StringToHash("attackHash");
+    int idleHash = Animator.StringToHash("idleHash");
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(attackHash);
+        }
+        else
+        {
+            anim.SetTrigger(idleHash);
+        }
+      
+	}
+}
